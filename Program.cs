bcb14c0d03a1385c58b7766c005283da9e341d7f
Jesper Raundahl <zacban@gmail.com>
2024-09-30 10:38:00 +0200

    Demonstrating Coupling

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class UserInterface
+{
+    private string _username;
+    private string _password;
+
+    private void GetData()
+    {
+        Console.WriteLine("Enter your username: ");
+        _username = Console.ReadLine();
+        Console.WriteLine("Enter your password: ");
+        _password = Console.ReadLine();
+    }
+
+    //sign up
+    public void SignUp()
+    {
+        GetData();
+        Business business = new Business();
+        business.SignUp(_username, _password);
+    }
+}
+
+public class Business {
+    public void SignUp(string username, string password) {
+        DataAccess dataAccess = new DataAccess();
+        dataAccess.SignUp(username, password);
+    }
+}
+
+public class DataAccess {
+
+    public void SignUp(string username, string password) {
+        Console.WriteLine("User signed up successfully");
+    }
+}
